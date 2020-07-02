250aaebd5c975456a65a43be3cdc95a0b90945b2
Alexander Marchenko <bourne-sci-hack@yandex.ru>
2020-07-03 03:17:59 +1000

    Redesign config
    Delete old files

--- .emacs.d/elpa/buffer-expose-0.4.3/buffer-expose-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "buffer-expose" "0.4.3" "Visual buffer switching using a window grid" '((emacs "25") (cl-lib "0.5")) :keywords '("convenience") :authors '(("Clemens Radermacher" . "clemera@posteo.net")) :maintainer '("Clemens Radermacher" . "clemera@posteo.net") :url "https://github.com/clemera/buffer-expose")
