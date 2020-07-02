d61dcf0840d8688cde50c58f40449a6fc67efe7c
Alexander Marchenko <bourne-sci-hack@yandex.ru>
2020-07-03 03:40:10 +1000

    User aliases

---
+++ config/aliases.el
@@ -0,0 +1,13 @@
+;;
+;; User aliases
+;;
+
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(defalias 'lisp       'lisp-mode)
+(defalias 'web         'web-mode)
+(defalias 'ss 'shell-script-mode)
+
+
+(provide 'aliases)
