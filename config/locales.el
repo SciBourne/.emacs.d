30afa07163c8e4694988f18e0970a16e94e7a7ef
Alexander Marchenko <bourne-sci-hack@yandex.ru>
2020-07-03 03:50:06 +1000

    User locale setting

---
+++ config/locales.el
@@ -0,0 +1,15 @@
+;;
+;; User locales
+;;
+
+
+(set-input-method "russian-computer")
+
+(setq locale-coding-system    'utf-8)
+(set-language-environment     'utf-8)
+(set-default-coding-systems   'utf-8)
+(set-terminal-coding-system   'utf-8)
+(prefer-coding-system         'utf-8)
+
+
+(provide 'locales)
