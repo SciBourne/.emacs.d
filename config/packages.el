9aa7fc4308570aae1dd78ce5501f933a8a26051d
Alexander Marchenko <bourne-sci-hack@yandex.ru>
2020-12-15 06:54:44 +1000

    Update packages.el

--- config/packages.el
+++ config/packages.el
@@ -10,27 +10,36 @@
 ;; Package list:
 (setq cfg-var:packages '(use-package
 
-			  ;for themes:
-			  kaolin-themes
-			  all-the-icons
-			  dashboard
-			  treemacs
-			  treemacs-projectile
-			  treemacs-icons-dired
-			  telephone-line
-			  svg-mode-line-themes
+			 ;for themes:
+			 kaolin-themes
+			 all-the-icons
+			 dashboard
+			 treemacs
+			 treemacs-projectile
+			 treemacs-icons-dired
+			 telephone-line
+			 svg-mode-line-themes
 
-			  ;for programming:
-			  highlight-indent-guides
-			  auto-complete
-			  projectile
-			  company
-			  swiper
-			  slime
-			  magit
-			  live-preview
-			  multiple-cursors
-			  smartparens))
+			 ;for programming:
+			 highlight-indent-guides
+			 highlight-parentheses
+			 highlight-numbers
+			 highlight
+			 auto-complete
+			 scroll-on-drag
+			 projectile
+			 company
+			 swiper
+			 slime
+			 elpy
+			 magit
+			 live-preview
+			 multiple-cursors
+			 pyenv-mode-auto
+			 pyenv-mode
+			 smartparens
+			 py-autopep8
+			 flycheck))
 
 
 ;; Installing packages if someone else is not installed:
@@ -45,7 +54,7 @@
 
 
 ;; Repositories:
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("gnu" .   "https://elpa.gnu.org/packages/")     t)
 (add-to-list 'package-archives '("org" .   "http://orgmode.org/elpa/")           t)
 
