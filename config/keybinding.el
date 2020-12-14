88074bbc3fb4405ab3bc4031d470b892b6349c5a
Alexander Marchenko <bourne-sci-hack@yandex.ru>
2020-12-15 06:54:29 +1000

    Update keybinding.el

--- config/keybinding.el
+++ config/keybinding.el
@@ -3,12 +3,37 @@
 ;;
 
 
-;; For multiple-cursors-mode:
-(global-set-key (kbd "C-c C-,") 'mc/edit-lines)
+;; hi-lock-mode:
+(if (functionp 'global-hi-lock-mode)
+            (global-hi-lock-mode 1)
+          (hi-lock-mode 1))
 
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+;; For multiple-cursors-mode:
+(global-set-key [M-S-mouse-3] 'mc/mark-all-symbols-like-this-in-defun)
+(global-set-key [M-S-mouse-2] 'mc/mark-all-symbols-like-this)
+(global-set-key [M-S-mouse-1] 'mc/add-cursor-on-click)
+(global-set-key (kbd "M--") 'mc/mark-next-like-this)
+
+
+;; Pythom mode:
+(global-set-key (kbd "M-.") 'jedi:goto-definition)
+(global-set-key (kbd "M-,") 'jedi:goto-definition-pop-marker)
+(global-set-key (kbd "M-/") 'jedi:show-doc)
+(global-set-key (kbd "M-?") 'helm-jedi-related-names)
+(global-set-key
+ (kbd "C-c f") (lambda ()
+		 (interactive)
+		 (shell-command (concat "C:/ProgramData/Miniconda3/python.exe " "\"" buffer-file-name "\""))))
+
+
+;; Scrolling:
+(global-set-key
+  [down-mouse-2]
+  (lambda ()
+    (interactive)
+    (unless (scroll-on-drag)
+      (mouse-yank-primary t))))
 
 
 (provide 'keybinding)
