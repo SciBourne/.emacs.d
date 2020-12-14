7aa2498f02cfeb02059b9c7e7ec75acdf792fc07
Alexander Marchenko <bourne-sci-hack@yandex.ru>
2020-12-15 06:54:40 +1000

    Update locales.el

--- config/locales.el
+++ config/locales.el
@@ -6,7 +6,7 @@
 (set-input-method "russian-computer")
 
 (setq locale-coding-system    'utf-8)
-(set-language-environment     'utf-8)
+(set-language-environment     'Russian)
 (set-default-coding-systems   'utf-8)
 (set-terminal-coding-system   'utf-8)
 (prefer-coding-system         'utf-8)
