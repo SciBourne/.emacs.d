95aa63e428d9d45e1d8b1f5fd03270b8016bf189
SciBourne <bourne-sci-hack@yandex.ru>
2021-05-03 13:51:08 +1000

    REFACTOR: Style refactoring

--- config/locales.el
+++ config/locales.el
@@ -1,15 +1,26 @@
-;;
-;; User locales
-;;
+;;;;
+;;;; User locales
+;;;;
+
+
+;;; Input method:  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 (set-input-method "russian-computer")
+
+
+;;;  Coding systems:  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 (setq locale-coding-system    'utf-8)
 (set-language-environment     'Russian)
 (set-default-coding-systems   'utf-8)
 (set-terminal-coding-system   'utf-8)
 (prefer-coding-system         'utf-8)
+
+
+;;;  END  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 
 (provide 'locales)
