c4f298b1d720719be20ce311a11ca5a7a41af377
SciBourne <bourne-sci-hack@yandex.ru>
2021-05-03 13:59:30 +1000

    REFACTOR: Style refactoring

--- config/irc.el
+++ config/irc.el
@@ -1,26 +1,63 @@
-;;
-;; User setting for RCIRC client
-;;
+;;;;
+;;;; User setting for RCIRC client
+;;;;
 
 
-;; Rcirc package setting:
+;;;  Rcirc package setting:  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 (setq rcirc-server-alist
 
       '(("irc.freenode.net"
 	 :port 6697
 	 :encryption tls
 	 :channels
-	 ("#lisp <#other-channel>")
-	 :nick "<nick>"
-	 :user-name "<User Name>"
-	 :full-name "<Full Name>"
-	 :password "<nick>:<password>")
+	 ("#lisp #emacs")
+	 :nick "Jenkins"
+	 :user-name "Leroy"
+	 :full-name "Leroy Jenkins"
+	 :password "VerySecretPassword")))
 
-	 ;for i2p:
-	("localhost"
-	 :port 6668
-	 :channels ("#i2p-dev <#other-channel>")
-	 :nick "<nick>")))
+
+;;;  Bright niks:  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(custom-set-variables
+ '(rcirc-bright-nicks '("your-bro-1" "your-bro-2")))
+
+
+;;;  Highlight nicks and server messages:  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;; TODO: Rebuild color theme:
+(custom-set-faces
+ '(rcirc-my-nick                    ((t (:foreground "medium aquamarine"))))
+ '(rcirc-nick-in-message-full-line '((t (:foreground "light blue"
+					 :weight bold))))
+
+ '(rcirc-other-nick                 ((t (:foreground "antique white"))))
+ '(rcirc-server                     ((t (:foreground "slate gray"))))
+
+ '(rcirc-timestamp                  ((t (:inherit default
+					 :foreground "sky blue")))))
+
+
+(put 'upcase-region 'disabled nil)
+
+
+(custom-set-faces
+ '(rcirc-my-nick                    ((t (:foreground "medium aquamarine"))))
+ '(rcirc-nick-in-message-full-line '((t (:foreground "light blue"
+					 :weight bold))))
+
+ '(rcirc-other-nick                 ((t (:foreground "antique white"))))
+ '(rcirc-server                     ((t (:foreground "slate gray"))))
+ '(rcirc-timestamp                  ((t (:inherit default
+					 :foreground "sky blue")))))
+
+
+;;;  END  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 
 (provide 'irc)
